7b47610d8c3d5925d000dc937f0bb13dffc94364
LB1304 <95583840+LB1304@users.noreply.github.com>
2022-09-10 20:00:39 +0200

    Add files via upload

---
+++ Application/Analysis.R
@@ -0,0 +1,114 @@
+## 1. Dataset description 
+
+require(igraph)
+bip_net <- read.csv("sandi_graph.csv", header = T, sep = " ")
+
+# Construct the co-authorship bipartite network
+Authors <- sort(unique(bip_net$Author))     # Id of authors
+Papers <- sort(unique(bip_net$Paper))       # Id of papers
+g <- graph.empty()
+g <- add.vertices(g, nv = length(Authors), attr = list(name = Authors, type = rep(TRUE, length(Authors))))      # Add the first layer of nodes (authors)
+g <- add.vertices(g, nv = length(Papers), attr = list(name = Papers, type = rep(FALSE, length(Papers))))        # Add the second layer of nodes (papers)
+edgeListVec <- as.vector(t(as.matrix(data.frame(Author = bip_net$Author, Paper = bip_net$Paper))))
+g <- add.edges(g, edgeListVec)                                                                                  # Add the edges
+
+# Select the main connected component
+cl <- components(g)                                         # Create the connected components of the graph
+vert_ids <- V(g)[cl$membership == which.max(cl$csize)]      # Select the main connected component
+g_main <- induced_subgraph(g, vert_ids)                     # Build the corresponding bipartite graph
+
+# Create the hypergraph in which nodes are authors and each hyperedge links the authors of a same paper
+# Discard the hyperedges of sizes 1 and 5
+A <- as_incidence_matrix(g_main)                    # Obtain the incidence matrix
+A <- A[-1, ]                                        # Remove the paper with 5 authors
+A_mod <- A[, -which(colSums(A) == 0)]
+A_mod <- A_mod[which(rowSums(A_mod) > 1), ]         # Select only papers with more than 1 author
+
+hyperedges <- vector(mode = "list", length = nrow(A_mod))
+for (i in 1:nrow(A_mod)) {
+  ind <- unname(which(A_mod[i, ] == 1))
+  hyperedges[[i]] <- colnames(A_mod)[ind]         # Store the hyperedges in a list
+}
+hyperedges <- unique(hyperedges)                    # Remove repeated hyperedges
+
+sink("./HG_coauth.txt")
+for (i in 1:length(hyperedges)) {
+  cat(paste(hyperedges[[i]], collapse = ","))     # Print the hyperedges to .txt file
+  cat("\n")
+}
+sink()
+
+
+
+## 2. Analysis with HyperSBM
+
+# Fit the HSBM model with Q=2,3,4, and two different initialization strategies: random and with soft spectral clustering
+require(HyperSBM)
+HG <- HyperSBM::import_hypergraph(file_name = "./HG_coauth.txt", method = "full")
+
+for (q in 2:4) {
+  res_rand <- HyperSBM::hSBM_par(Hypergraph = HG, Q = q, start = 0, model = 0, tol = 1e-5, maxit_VEM = 100, maxit_FP = 100, n_threads = 30, print = TRUE)
+  res_ssc <- HyperSBM::hSBM_par(Hypergraph = HG, Q = q, start = 2, model = 0, tol = 1e-5, maxit_VEM = 100, maxit_FP = 100, n_threads = 30, print = TRUE)
+  save(res_rand, res_ssc, file = paste0("res_coauth_Q", q, ".RData"))
+
+load("./res_coauth_Q2.RData")
+table(res_rand$Z)                   # Frequency table in the 2 groups
+small <- unname(which.min(table(res_rand$Z)))
+
+num_co_auth <- function(colonna) {              # Function to compute the number of co-authors of a given author (specified by its column index in matrix A_mod)
+  pap <- which(A_mod[, colonna] != 0)
+  co_auth <- c()
+  for (p in pap) {
+    co_auth <- c(co_auth, which(A_mod[p, ] != 0))
+  }
+  return(length(unique(co_auth))-1)
+}
+
+grp1_names <- names(colSums(A_mod[, which(res_rand$Z == small)]))       # Id of the authors in small group 1
+grp1_ind <- which(colnames(A_mod) %in% grp1_names)                      # Column index in matrix A_mod of the authors in small group 1
+
+table(unlist(lapply(1:ncol(A_mod), num_co_auth)))   # Distribution of the number of distinct co-authors per author 
+unlist(lapply(grp1_ind, num_co_auth))               # Number of distinct co-authors for the 6 athors in small group 1
+
+table(colSums(A_mod))                               # Degree distribution of authors
+colSums(A_mod[, which(res_rand$Z == small)])        # Degree of the 6 authors in small group 1
+
+# Inspect the values of the estimated probabilities of hyperedges occurrance
+res_rand$B          # Estimates for the probability of occurrance of an hyperedge
+
+
+## 3. Comparison with two other methods
+
+# Compare our approach with the spectral clustering algorithm
+SpectralClust <- function (L, Q, n) {                       # Estimation function for Spctral Clustering
+  X <- as.matrix(eigen(L)$vectors[, (n - Q + 1):n])
+  X <- X/rowSums(X)
+  km <- kmeans(x = X, centers = Q, nstart = 100)
+  
+  return(km$cluster)
+}
+res_sc <- SpectralClust(L = HG$Laplacian, Q = 2, n = HG$Num_nodes)
+
+table(res_sc)               # Frequency table in tha groups
+small <- unname(which.min(table(res_sc)))
+
+grp1_sc_names <- colnames(A_mod)[which(res_sc == small)]    # Id of the authors in small group 1
+grp1_sc_ind <- which(colnames(A_mod) %in% grp1_sc_names)    # Column index in matrix A_mod of the authors in small group 1
+
+colSums(A_mod[, which(res_sc == small)])        # Degree of the authors in the first group
+unlist(lapply(grp1_sc_ind, num_co_auth))        # Number of distinct co-authors for the authors in the smaller group
+
+# Analyze the bipartite graph with the R package sbm with option `bipartite`
+require(sbm)
+res_bp <- estimateBipartiteSBM(A_mod)       # Estimation function for Bipartite SBM
+table(res_bp$memberships$col)               # Frequency table in tha groups
+small <- unname(which.min(table(res_bp$memberships$col)))
+
+grp1_bp_names <- colnames(A_mod)[which(res_bp$memberships$col == small)]    # Id of the authors in small group 1
+grp1_bp_ind <- which(colnames(A_mod) %in% grp1_bp_names)                    # Column index in matrix A_mod of the authors in small group 1
+
+colSums(A_mod[, which(res_bp$memberships$col == small)])    # Degree of the authors in the first group
+unlist(lapply(grp1_bp_ind, num_co_auth))                    # Number of distinct co-authors for the authors in the smaller group
+
+
+
